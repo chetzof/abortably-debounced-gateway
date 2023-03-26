afc378a2944f281e9a3a48575c82a57c51e59717
Vlad Cos <vcosvic@gmail.com>
2023-03-27 00:38:46 +0300

    Initial commit

---
+++ .lintstagedrc.js
@@ -0,0 +1,19 @@
+const { ESLint } = require('eslint')
+
+const removeIgnoredFiles = async (files) => {
+  const eslint = new ESLint()
+  const isIgnored = await Promise.all(
+    files.map((file) => {
+      return eslint.isPathIgnored(file)
+    }),
+  )
+  const filteredFiles = files.filter((_, i) => !isIgnored[i])
+  return filteredFiles.join(' ')
+}
+module.exports = {
+  '*': async (files) => {
+    const filesToLint = await removeIgnoredFiles(files)
+    return [`npm run fix:eslint ${filesToLint}`, 'npm run fix:prettier .']
+  },
+  '**/*.ts?(x)': [() => 'npm run lint:tsc', 'vitest related --run'],
+}
